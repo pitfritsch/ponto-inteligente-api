2858a6f7b7cf395247050d723ab08ac2a933769f
Pedro Fritsch <pedrofritsch98@gmail.com>
2020-06-17 17:27:13 -0300

    criado entidades JPA

--- src/main/java/com/kazale/pontointeligente/DemoApplication.java
+++ src/main/java/com/kazale/pontointeligente/DemoApplication.java
@@ -1,13 +1,14 @@
 package com.kazale.pontointeligente;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("TEste");
 	}
 
 }
